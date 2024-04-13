54fba3e3c4ff23b9e02b7b4074efec7f37697448
Aday <abdirizakgulet@gmail.com>
2024-04-13 18:18:48 +0300

    done lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName='bob'
+function upperCaseCustomerName(){
+    if(typeof customerName!=='undefined'){
+        customerName=customerName.toUpperCase();
+    } else{
+        console.log('customerName is not defined globally');
+            
+    }
+}
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+function overwriteBestCustomer(){
+bestCustomer='maybe bob';
+
+}
+
+
+
+const leastFavoriteCustomer='John';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='Alice';
+}
